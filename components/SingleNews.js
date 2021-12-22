f201546d98f3a9ad1b56463d15733599afe30c92
ÜMİT AKTAŞ <umitaktass@hotmail.com>
2021-12-22 19:35:50 +0300

    create inshort tabs and singlenews

---
+++ components/SingleNews.js
@@ -0,0 +1,78 @@
+import React from "react";
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  Linking,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
+const SingleNews = ({ item, index, darkTheme }) => {
+  return (
+    <View
+      style={{
+        height: windowHeight,
+        width: windowWidth,
+        transform: [{ scaleY: -1 }],
+      }}
+    >
+      <Image
+        source={{ uri: item.urlToImage }}
+        style={{ height: "45%", resizeMode: "cover", width: windowWidth }}
+      />
+      <View
+        style={{
+          ...styles.description,
+          backgroundColor: darkTheme ? "#282C35" : "white",
+        }}
+      >
+        <Text style={{ ...styles.title, color: darkTheme ? "white" : "black" }}>
+          {item.title}
+        </Text>
+        <Text
+          style={{ ...styles.content, color: darkTheme ? "white" : "black" }}
+        >
+          {item.description}
+        </Text>
+        <Text style={{ color: darkTheme ? "white" : "black" }}>
+          Yazar:
+          <Text style={{ fontWeight: "bold" }}>
+            {" "}
+            {item.author ?? "unknown"}
+          </Text>
+        </Text>
+      </View>
+      
+    </View>
+  );
+};
+
+export default SingleNews;
+
+const styles = StyleSheet.create({
+  description: {
+    padding: 15,
+    flex: 1,
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: "bold",
+    paddingBottom: 10,
+  },
+  content: { fontSize: 18, paddingBottom: 10 },
+  footer: {
+    height: 80,
+    width: windowWidth,
+    position: "absolute",
+    bottom: 0,
+    backgroundColor: "#d7be69",
+    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+});
